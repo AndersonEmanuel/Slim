d55389c0ce22d23d5455ba5ffd18c93337159e86
Anderson Emanuel <andersonemanuel.s@gmail.com>
2018-04-27 13:36:19 -0300

    Installing the e-mail library

---
+++ src/Application/Mail/Mailer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Application\Mail;
+
+/**
+ * 
+ * Description of Mailer
+ * 
+ * @package Application
+ * @author Anderson Emanuel <contato@andersonemanuel.com.br>
+ * @copyright (c) 2018, Anderson Emanuel
+ * @version 1.0
+ */
+class Mailer {
+
+}
